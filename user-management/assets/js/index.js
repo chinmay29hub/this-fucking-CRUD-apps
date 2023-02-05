3427e8b2ff49bf8717d3337ebfee09d59154a460
chinmay29hub <chinmaysonawane57@gmail.com>
2023-02-05 20:29:29 +0530

    fix bugs

--- user-management/assets/js/index.js
+++ user-management/assets/js/index.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv"
+dotenv.config({path : "../../config.env"})
+
+
 $("#add_user").submit(function(event){
     alert("user Created Successfully")
 })
--- user-management/server/services/render.js
+++ user-management/server/services/render.js
@@ -1,4 +1,6 @@
 const axios = require("axios")
+const dotenv = require('dotenv')
+dotenv.config({path : "../../config.env"})
 
 
 exports.homeRoutes = (req, res) => {
