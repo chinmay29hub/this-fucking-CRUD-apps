5b66fdeae96a65b1364b09c24429ed246cf76b34
chinmay29hub <chinmaysonawane57@gmail.com>
2023-02-11 21:06:23 +0530

    todo

---
+++ todo-app/api/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const Todo = require("./model/Todo")
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ })
+    .then(() => {
+        console.log("ATLAS database Connected")
+    }).catch(console.error)
+
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find()
+
+    res.json(todos)
+})
+
+app.post("/todo/new", (req, res) => {
+    const todo = new Todo({
+        text : req.body.text
+    })
+
+    todo.save()
+
+    res.json(todo)
+})
+
+app.delete("/todo/delete/:id", async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id)
+
+    res.json(result)
+})
+
+app.put("/todo/complete/:id", async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+
+    todo.complete = !todo.complete
+
+    todo.save()
+
+    res.json(todo)
+})
+
+app.listen(3001, () => {
+    console.log(`Server Started at port ${3001}`)
+})
+
