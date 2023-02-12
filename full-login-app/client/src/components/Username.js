be9d6d34a8f70b3927aa995ded2e7edc6e7d1344
chinmay29hub <chinmaysonawane57@gmail.com>
2023-02-12 12:14:05 +0530

    optimised fucking api

--- full-login-app/client/src/components/Username.js
+++ full-login-app/client/src/components/Username.js
@@ -40,9 +40,9 @@ export default function Username() {
         <div className='flex justify-center items-center h-screen'>
             <div className={styles.glass}>
                 <div className='title flex flex-col items-center'>
-                    <h4 className='text-5xl font-bold'>hello Again</h4>
+                    <h4 className='text-5xl font-bold'>Hello Again</h4>
                     <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
-                        Some dummy text
+                        Lets test this Login APP
                     </span>
                 </div>
                 <form className='py-1' onSubmit={formik.handleSubmit}>
--- full-login-app/server/database/connection.js
+++ full-login-app/server/database/connection.js
@@ -1,12 +1,11 @@
-import mongoose, { mongo } from "mongoose"
-import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
 import ENV from "../config.js"
 // const mongoose = require('mongoose');
 // const MongoMemoryServer = require('mongodb-memory-server');
 
 async function connect() {
-    const mongod = await MongoMemoryServer.create()
-    const getUri = mongod.getUri()
+    // const mongod = await MongoMemoryServer.create()
+    // const getUri = mongod.getUri()
     mongoose.set("strictQuery", true)
     // const db = await mongoose.connect(getUri)
     const db = await mongoose.connect(ENV.ATLAS_URI)
