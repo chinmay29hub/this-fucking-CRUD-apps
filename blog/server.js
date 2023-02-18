551bc117ce5c49600aeb3ffdb7ed0198f55482e3
chinmay29hub <chinmaysonawane57@gmail.com>
2023-02-18 11:58:58 +0530

    blog app ejs

---
+++ blog/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require("cors")
+const dotenv = require('dotenv');
+const mongoose = require("mongoose")
+const articleRouter = require("./routes/articles")
+const Article = require("./models/article")
+const methodOverride = require('method-override');
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended : false }))
+app.use(methodOverride("_method"))
+
+app.use("/articles",articleRouter)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.ATLAS_URI).then(() => {
+    console.log("Connected to Atlas")
+}).catch(error => console.log(error))
+
+app.get("/", async (req, res) => {
+    // res.send("hello world")
+    const articles = await Article.find().sort({ createdAt : "desc" })
+    res.render("articles/index", { articles : articles })
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log("Server Started on 4000")
+})
